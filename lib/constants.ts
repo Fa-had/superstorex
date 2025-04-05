1df149f6935369d5742ff9b557d783e8f1ec4b4f
Fa-had <rjfahad01718@gmail.com>
2025-04-06 02:13:57 +0600

    layout configured

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME =
+  process.env.NEXT_PUBLIC_APP_NAME || 'value NULL, CHECK lib/constants.ts'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'value NULL, CHECK lib/constants.ts'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'value NULL, CHECK lib/constants.ts'
