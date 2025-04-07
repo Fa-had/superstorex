c995a9a4e10947594918428d08ab3b5f3df36d58
Fa-had <rjfahad01718@gmail.com>
2025-04-07 16:18:26 +0600

    homepage banner display part with carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,5 +33,28 @@ const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'Most Popular ESP32 Camera For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/Banner_ESP32-CAM.webp',
+      url: '/search?category=Modeule',
+      isPublished: true,
+    },
+    {
+      title: 'Most Popular Luckfox pico For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/Luckfox-Pico-M.webp',
+      url: '/search?category=Development-Board',
+      isPublished: true,
+    },
+    {
+      title: 'Most Popular Arduino For Sale',
+      buttonCaption: 'See More',
+      image: '/images/Arduino_Banner.webp',
+      url: '/search?category=Develipment-Board',
+      isPublished: true,
+    },
+  ],
 }
 export default data
