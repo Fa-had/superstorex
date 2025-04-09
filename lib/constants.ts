3107b8aa4405da789b614925c9cd721c4ff6cb67
Fa-had <rjfahad01718@gmail.com>
2025-04-09 22:03:23 +0600

    created product details page

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,3 +5,5 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'value NULL, CHECK lib/constants.ts'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
